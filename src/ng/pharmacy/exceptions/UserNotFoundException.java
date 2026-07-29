6d4f62966a834ad195a28a927cdb795a8a8ef18a
Isaac Urban <kpomassiurban@gmail.com>
2026-07-30 00:37:03 +0100

    created user not found exception

---
+++ src/ng/pharmacy/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ng.pharmacy.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
