67e385e60e6de09a98088492e0e548f656409789
Isaac Urban <kpomassiurban@gmail.com>
2026-07-30 00:36:01 +0100

    updated mapper to include request and response for add medicine

--- src/ng/pharmacy/utils/Mapper.java
+++ src/ng/pharmacy/utils/Mapper.java
@@ -1,11 +1,13 @@
 package ng.pharmacy.utils;
 
+import ng.pharmacy.data.models.Medicine;
 import ng.pharmacy.data.models.User;
-import ng.pharmacy.dto.request.LoginChemistRequest;
-import ng.pharmacy.dto.request.RegisterChemistRequest;
-import ng.pharmacy.dto.response.LoginChemistResponse;
-import ng.pharmacy.dto.response.LogoutChemistResponse;
-import ng.pharmacy.dto.response.RegisterChemistResponse;
+import ng.pharmacy.dto.request.MedicineServiceRequests.AddMedicineRequest;
+import ng.pharmacy.dto.request.authServiceRequests.RegisterChemistRequest;
+import ng.pharmacy.dto.response.authServiceResponse.LoginChemistResponse;
+import ng.pharmacy.dto.response.authServiceResponse.LogoutChemistResponse;
+import ng.pharmacy.dto.response.authServiceResponse.RegisterChemistResponse;
+import ng.pharmacy.dto.response.medicineServiceResponse.AddMedicineResponse;
 
 public class Mapper {
 
@@ -34,6 +36,36 @@ public class Mapper {
     public static LogoutChemistResponse mapLogoutToUserResponse(User user){
         LogoutChemistResponse response = new LogoutChemistResponse();
         response.setLoggedIn(user.isLoggedIn());
+        return response;
+    }
+
+    public static Medicine mapToMedicine(AddMedicineRequest request) {
+
+        Medicine medicine = new Medicine();
+
+        medicine.setBrandName(request.getBrandName());
+        medicine.setGenericName(request.getGenericName());
+        medicine.setStrength(request.getStrength());
+        medicine.setDosageForm(request.getDosageForm());
+        medicine.setManufacturer(request.getManufacturer());
+        medicine.setBatchNumber(request.getBatchNumber());
+        medicine.setManufactureDate(request.getManufactureDate());
+        medicine.setExpiryDate(request.getExpiryDate());
+        medicine.setUnitPrice(request.getUnitPrice());
+        medicine.setQuantityInStock(request.getQuantityInStock());
+
+        return medicine;
+    }
+
+    public static AddMedicineResponse mapToMedicineResponse(Medicine medicine) {
+
+        AddMedicineResponse response = new AddMedicineResponse();
+
+        response.setId(medicine.getId());
+        response.setBrandName(medicine.getBrandName());
+        response.setGenericName(medicine.getGenericName());
+        response.setQuantityInStock(medicine.getQuantityInStock());
+
         return response;
     }
 }
