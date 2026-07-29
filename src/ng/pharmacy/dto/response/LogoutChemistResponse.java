60511c6fd230814080c5fea51a68bd30d55353a6
Isaac Urban <kpomassiurban@gmail.com>
2026-07-29 15:04:57 +0100

    did my respense for logout

--- src/ng/pharmacy/dto/response/LogoutChemistResponse.java
+++ src/ng/pharmacy/dto/response/LogoutChemistResponse.java
@@ -2,4 +2,13 @@ package ng.pharmacy.dto.response;
 
 public class LogoutChemistResponse {
 
+    private boolean isLoggedIn;
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
 }
