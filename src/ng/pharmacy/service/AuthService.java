98126f726dee013b73ee0ad6f0fb3644dedc1291
Isaac Urban <kpomassiurban@gmail.com>
2026-07-29 15:03:50 +0100

    Added logout logic

--- src/ng/pharmacy/service/AuthService.java
+++ src/ng/pharmacy/service/AuthService.java
@@ -1,13 +1,15 @@
 package ng.pharmacy.service;
 
 import ng.pharmacy.dto.request.LoginChemistRequest;
+import ng.pharmacy.dto.request.LogoutChemistRequest;
 import ng.pharmacy.dto.request.RegisterChemistRequest;
 import ng.pharmacy.dto.response.LoginChemistResponse;
+import ng.pharmacy.dto.response.LogoutChemistResponse;
 import ng.pharmacy.dto.response.RegisterChemistResponse;
 import ng.pharmacy.exceptions.InvalidPasswordException;
 
 public interface AuthService {
     RegisterChemistResponse registerChemist(RegisterChemistRequest request);
     LoginChemistResponse loginChemist(LoginChemistRequest request) throws InvalidPasswordException;
-    LogOutUserResponse logoutChemist(LogoutChemsistRequest request);
+    LogoutChemistResponse logoutChemist(LogoutChemistRequest request);
 }
