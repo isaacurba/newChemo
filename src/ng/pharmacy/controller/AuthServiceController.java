596baa61e9ac28bc7fd0d342b04108e253e6201c
Isaac Urban <kpomassiurban@gmail.com>
2026-07-31 13:48:00 +0100

    done some dependency injection

--- src/ng/pharmacy/controller/AuthServiceController.java
+++ src/ng/pharmacy/controller/AuthServiceController.java
@@ -1,14 +1,34 @@
 package ng.pharmacy.controller;
 
+import ng.pharmacy.data.repositories.UserRepository;
+import ng.pharmacy.dto.request.authServiceRequests.LoginChemistRequest;
+import ng.pharmacy.dto.request.authServiceRequests.LogoutChemistRequest;
 import ng.pharmacy.dto.request.authServiceRequests.RegisterChemistRequest;
+import ng.pharmacy.dto.response.authServiceResponse.LoginChemistResponse;
+import ng.pharmacy.dto.response.authServiceResponse.LogoutChemistResponse;
 import ng.pharmacy.dto.response.authServiceResponse.RegisterChemistResponse;
+import ng.pharmacy.exceptions.InvalidPasswordException;
 import ng.pharmacy.service.AuthService;
 import ng.pharmacy.service.AuthServiceImp;
 
 public class AuthServiceController {
-    private final AuthService authService = new AuthServiceImp();
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    public AuthServiceController(AuthService authService, UserRepository userRepository) {
+        this.authService = authService;
+        this.userRepository = userRepository;
+    }
 
     public RegisterChemistResponse registerChemist(RegisterChemistRequest request){
         return authService.registerChemist(request);
+    }
+
+    public LoginChemistResponse loginChemist(LoginChemistRequest request) throws InvalidPasswordException {
+        return authService.loginChemist(request);
+    }
+
+    public LogoutChemistResponse logoutChemist(LogoutChemistRequest request){
+        return authService.logoutChemist(request);
     }
 }
